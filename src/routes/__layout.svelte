2b502d7a0d414bc0ea4851af2aa21e8491558d94
Phil Middleton <middleton.philip.pm@gmail.com>
2021-10-07 13:34:26 -0600

    commit

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,15 @@
-<script>import "../app.css";</script>
+<script>
+import Nav from "$lib/nav.svelte";
+import { themeChange} from "theme-change"
+import { onMount } from "svelte";
+import "../app.css";
 
+onMount(async () => {
+  themeChange(false)
+})
+</script>
+
+<Nav />
 <main class="container max-w-xl mx-auto px-4">
   <slot></slot>
 </main>
